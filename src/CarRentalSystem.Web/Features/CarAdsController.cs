581e32a5fd338b9c5203202363718572ea5632e6
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-05-30 20:08:50 +0300

    Introduce a Presentation Layer

---
+++ src/CarRentalSystem.Web/Features/CarAdsController.cs
@@ -0,0 +1,20 @@
+﻿namespace CarRentalSystem.Web.Features
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Domain.Models.CarAds;
+    using Domain.Models.Dealers;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CarAdsController : ControllerBase
+    {
+        private static readonly Dealer Dealer = new Dealer("Dealer", "+12345678");
+
+        [HttpGet]
+        public IEnumerable<CarAd> Get() => Dealer
+            .CarAds
+            .Where(c => c.IsAvailable);
+    }
+}
